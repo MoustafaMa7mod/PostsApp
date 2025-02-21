80e27f20515a5e9760596917a41a5405f046c77a
Moustafa Mahmoud <mostafa.akrap@gmail.com>
2025-02-21 17:07:09 +0200

    add initial structure for project

---
+++ PostsApp/AppDelegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PostsApp
+//
+//  Created by Moustafa on 21/02/2025.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
