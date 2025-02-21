7413d0a2c2ab6239666a9e17da19b6a22b8b598b
Moustafa Mahmoud <mostafa.akrap@gmail.com>
2025-02-21 18:04:08 +0200

    add post data model in entity

---
+++ PostsApp/Data/Models/PostDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  PostDataModel.swift
+//  PostsApp
+//
+//  Created by Moustafa on 21/02/2025.
+//
+
+struct PostDataModel: Codable {
+    
+    let userId: Int?
+    let id: Int?
+    let title: String?
+    let body: String?
+}
